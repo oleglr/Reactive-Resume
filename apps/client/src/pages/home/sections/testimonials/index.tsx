bf12ec4f32ad2380408429daceb110dcac4dee2b
OlegLR <oleglr@mail.ru>
2023-12-19 23:27:37 +0300

    first

--- apps/client/src/pages/home/sections/testimonials/index.tsx
+++ apps/client/src/pages/home/sections/testimonials/index.tsx
@@ -6,7 +6,7 @@ import { Quotes } from "@phosphor-icons/react";
 import { cn } from "@reactive-resume/utils";
 import { motion } from "framer-motion";
 
-const email = "hello@amruthpillai.com";
+const email = "oleglr@mail.ru";
 
 type Testimonial = {
   quote: string;
